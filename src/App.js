4fece21942564109cbd6c80d39d803e96d8adc0c
Alex Chesnay <me@alex-chesnay.fr>
2019-10-25 02:24:51 +0200

    feat: création des fonctionnalités basiques

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import React, { Fragment } from "react";
+import React, { Fragment, Component } from "react";
 import "./App.css";
+import sampleText from "./sampleText";
+import marked from "marked";
 
-function App() {
-  return (
-    <Fragment>
-      <h1>Éditeur Markdown</h1>
-    </Fragment>
-  );
+class App extends Component {
+  state = {
+    text: sampleText
+  };
+  handleChange = event => {
+    const text = event.target.value;
+    this.setState({ text });
+  };
+  renderMarkdown = text => {
+    const __html = marked(text, { sanitize: true });
+    return { __html };
+  };
+  render() {
+    return (
+      <Fragment>
+        <div className="container">
+          <h1>Éditeur Markdown</h1>
+          <hr />
+          <div className="row">
+            <div className="col-sm-6">
+              <textarea
+                rows="35"
+                className="form-control"
+                value={this.state.text}
+                onChange={this.handleChange}
+              ></textarea>
+            </div>
+            <div className="col-sm-6">
+              <div
+                dangerouslySetInnerHTML={this.renderMarkdown(this.state.text)}
+              ></div>
+            </div>
+          </div>
+        </div>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
---
+++ src/sampleText.js
@@ -0,0 +1,1 @@
+export default `# Titre de premier niveau\n## Titre de second niveau\n### Titre de troisième niveau\n#### Titre de quatrième niveau\n##### Titre de cinquième niveau\n###### Titre de sixième niveau\n[Lien](#)`;
